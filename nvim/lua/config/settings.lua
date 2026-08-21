1601c90e2932a4e283937d818a8b14aabf9fb61d
Håkon Anders Strømsodd <haakon8855@gmail.com>
2026-08-21 14:09:19 +0200

    nvim: reopen on last line and center view

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -5,3 +5,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function() vim.hl.on_yank() end,
 })
 
+-- Reopen file to same location as last time
+vim.api.nvim_create_autocmd('BufWinEnter', {
+    group = vim.api.nvim_create_augroup('last_place', { clear = true }),
+    callback = function(args)
+        if vim.bo[args.buf].filetype == 'gitcommit' then return end
+
+        local line = vim.api.nvim_buf_get_mark(args.buf, '"')[1]
+        if line > 0 and line <= vim.api.nvim_buf_line_count(args.buf) then
+            vim.cmd('normal! g`"zz')
+        end
+    end,
+})
