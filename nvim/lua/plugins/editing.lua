dfa3a6a211572703bf37703dbb16773f5031e43b
Håkon Anders Strømsodd <haakon8855@gmail.com>
2026-08-25 09:58:23 +0200

    nvimrc: change spider keybindings
    - Same change to .ideavimrc

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -11,10 +11,10 @@ return {
     {
         'chrisgrieser/nvim-spider',
         keys = {
-            { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
-            { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
-            { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
-            { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+            { "W", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+            { "E", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+            { "B", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+            { "gE", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
         },
     },
 }
